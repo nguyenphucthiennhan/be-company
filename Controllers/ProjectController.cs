2836705fad55831a6f08affa8f4d8f8ff8858fc9
thuytiendev <thuytien16102003@gmail.com>
2025-01-09 13:18:44 +0700

    new

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -51,34 +51,89 @@ namespace be_company.Controllers
         }
 
         // PUT: api/projects/{id}
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateProject(int id, Project project)
+[HttpPut("{id}")]
+public async Task<IActionResult> UpdateProject(int id, Project project)
+{
+    if (id != project.ProjectId)
+    {
+        return BadRequest();
+    }
+
+    // Find the existing project (without including ProjectDetails)
+    var existingProject = await _context.Projects
+        .FirstOrDefaultAsync(p => p.ProjectId == id);
+
+    if (existingProject == null)
+    {
+        return NotFound();
+    }
+
+    // Update the project fields
+    existingProject.ProjectName = project.ProjectName;
+    existingProject.StartDate = project.StartDate;
+    existingProject.EndDate = project.EndDate;
+    existingProject.Status = project.Status;
+    existingProject.ManagerID = project.ManagerID;
+
+    // Manually update the ProjectDetails if project.ProjectDetails is not null
+    if (project.ProjectDetails != null)
+    {
+        var existingProjectDetail = await _context.ProjectDetails
+            .Where(pd => pd.ProjectId == id) // Connect using ProjectId
+            .FirstOrDefaultAsync();
+
+        if (existingProjectDetail != null)
         {
-            if (id != project.ProjectId)
-            {
-                return BadRequest();
-            }
+            // Update the existing ProjectDetail
+            existingProjectDetail.DetailedDescription = project.ProjectDetails.DetailedDescription;
+            existingProjectDetail.EstimatedBudget = project.ProjectDetails.EstimatedBudget;
+            existingProjectDetail.ActualBudget = project.ProjectDetails.ActualBudget;
+            existingProjectDetail.SRS = project.ProjectDetails.SRS;
+            existingProjectDetail.ClientID = project.ProjectDetails.ClientID;
 
-            _context.Entry(project).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProjectExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            // Mark the ProjectDetail entity as modified
+            _context.Entry(existingProjectDetail).State = EntityState.Modified;
         }
+        else
+        {
+            // If ProjectDetails doesn't exist, create a new one
+            var newProjectDetail = new ProjectDetail
+            {
+                DetailedDescription = project.ProjectDetails.DetailedDescription,
+                EstimatedBudget = project.ProjectDetails.EstimatedBudget,
+                ActualBudget = project.ProjectDetails.ActualBudget,
+                SRS = project.ProjectDetails.SRS,
+                ClientID = project.ProjectDetails.ClientID,
+                ProjectId = id // Associate with the correct project
+            };
+
+            // Add the new ProjectDetail
+            _context.ProjectDetails.Add(newProjectDetail);
+        }
+    }
+
+    // Mark the project entity as modified
+    _context.Entry(existingProject).State = EntityState.Modified;
+
+    try
+    {
+        // Save changes to the database
+        await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!ProjectExists(id))
+        {
+            return NotFound();
+        }
+        else
+        {
+            throw;
+        }
+    }
+
+    return NoContent();
+}
 
         // DELETE: api/projects/{id}
         [HttpDelete("{id}")]
