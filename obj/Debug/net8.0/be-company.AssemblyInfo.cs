deaa6f84d80e6956f9dcb3905a7210f79ea4f9d3
thuytiendev <thuytien16102003@gmail.com>
2025-01-15 12:17:29 +0700

    nh

--- obj/Debug/net8.0/be-company.AssemblyInfo.cs
+++ obj/Debug/net8.0/be-company.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be-company")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e9066de657de310ff9adb5ad9a59a71ad70eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2836705fad55831a6f08affa8f4d8f8ff8858fc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("be-company")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be-company")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
